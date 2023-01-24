c4f35296290adcdf6be519644cd1e7d5fd165ade
M_EtOuais <28m06c02@gmail.com>
2023-01-24 23:10:00 +0100

    complete compare

--- src/CompareImage.cpp
+++ src/CompareImage.cpp
@@ -3,13 +3,25 @@
 #include "FreeImage.h"
 
 bool CompareImage::compare(const std::string& imgPath1, const std::string& imgPath2) {
+    RGBQUAD clr1, clr2;
     FIBITMAP *img1=nullptr, *img2=nullptr;
+    unsigned long long error=0;
     bool status=false;
     if (FreeImage_GetFIFCount()<=0) goto quit;
     img1 = FreeImage_Load(FIF_PNG, imgPath1.c_str(),0);
     if (!img1) goto quit;
     img2 = FreeImage_Load(FIF_PNG, imgPath2.c_str(),0);
     if (!img2) goto quit;
+    if (FreeImage_GetWidth(img1) != FreeImage_GetWidth(img2)) goto quit;
+    if (FreeImage_GetHeight(img1) != FreeImage_GetHeight(img2)) goto quit;
+    for (unsigned x=0; x<FreeImage_GetWidth(img1); x++) {
+        for (unsigned y=0; y<FreeImage_GetHeight(img1); y++) {
+            FreeImage_GetPixelColor(img1,x,y,&clr1);
+            FreeImage_GetPixelColor(img2,x,y,&clr2);
+            if (clr1.rgbRed != clr2.rgbRed || clr1.rgbGreen != clr2.rgbGreen || clr1.rgbBlue != clr2.rgbBlue) error++;
+        }
+    }
+    std::cout << ((error>1000) ? "KO" : "OK") << std::endl << error << std::endl;
     status=true;
 quit:
     FreeImage_Unload(img1);
