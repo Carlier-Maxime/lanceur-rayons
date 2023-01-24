6773b703a46deb5f5dce512d2ea443650e26f8de
M_EtOuais <28m06c02@gmail.com>
2023-01-24 21:12:36 +0100

    line separator : LF

--- compare.sh
+++ compare.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-IMAGE1=$1
-IMAGE2=$2
-
-# Pour connaître l'emplacement du script compare.sh
-MYPATH=$(dirname "$0")
-
-# java -cp $MYPATH/bin CompareImage $IMAGE1 $IMAGE2
-echo "Pas encore réalisé"
+bin/compareImage "$1" "$2"
