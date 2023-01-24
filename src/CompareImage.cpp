697b34bb4ce7241a2c919b20a87ebf3b18d3de2e
M_EtOuais <28m06c02@gmail.com>
2023-01-24 23:51:10 +0100

    create diff.png in compare function

--- src/CompareImage.cpp
+++ src/CompareImage.cpp
@@ -3,8 +3,8 @@
 #include "FreeImage.h"
 
 bool CompareImage::compare(const std::string& imgPath1, const std::string& imgPath2) {
-    RGBQUAD clr1, clr2;
-    FIBITMAP *img1=nullptr, *img2=nullptr;
+    RGBQUAD clr1, clr2, clrD;
+    FIBITMAP *img1=nullptr, *img2=nullptr, *diff=nullptr;
     unsigned long long error=0;
     bool status=false;
     if (FreeImage_GetFIFCount()<=0) {
@@ -29,13 +29,24 @@ bool CompareImage::compare(const std::string& imgPath1, const std::string& imgPa
         std::cerr << "Height image is different !" << std::endl;
         goto quit;
     }
+    diff = FreeImage_Allocate((int) FreeImage_GetWidth(img1), (int) FreeImage_GetHeight(img1), 24);
+    if (!diff) {
+        std::cerr << "failed allocate for diff.png !" << std::endl;
+    }
     for (unsigned x=0; x<FreeImage_GetWidth(img1); x++) {
         for (unsigned y=0; y<FreeImage_GetHeight(img1); y++) {
             FreeImage_GetPixelColor(img1,x,y,&clr1);
             FreeImage_GetPixelColor(img2,x,y,&clr2);
-            if (clr1.rgbRed != clr2.rgbRed || clr1.rgbGreen != clr2.rgbGreen || clr1.rgbBlue != clr2.rgbBlue) error++;
+            if (clr1.rgbRed != clr2.rgbRed || clr1.rgbGreen != clr2.rgbGreen || clr1.rgbBlue != clr2.rgbBlue) {
+                error++;
+                clrD.rgbRed = (clr1.rgbRed+clr2.rgbRed)/2;
+                clrD.rgbGreen = (clr1.rgbGreen+clr2.rgbGreen)/2;
+                clrD.rgbBlue = (clr1.rgbBlue+clr2.rgbBlue)/2;
+            } else clrD={0,0,0};
+            FreeImage_SetPixelColor(diff,x,y,&clrD);
         }
     }
+    FreeImage_Save(FIF_PNG, diff, "diff.png",0);
     std::cout << ((error>1000) ? "KO" : "OK") << std::endl << error << std::endl;
     status=true;
 quit:
