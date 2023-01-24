03a38fcea5b3a1608406c7642c5191e1c74c5dfc
M_EtOuais <28m06c02@gmail.com>
2023-01-24 23:12:49 +0100

    enable pipeline comparateur

--- tests.sh
+++ tests.sh
@@ -20,8 +20,9 @@ echo "Tests du comparateur d'images"
 assert "./compare.sh TEST1/image1.png TEST1/image1.png" "OK\n0\n"
 assert "./compare.sh TEST1/image2.png TEST1/image2.png" "OK\n0\n"
 assert "./compare.sh TEST1/image1.png TEST1/image2.png" "OK\n879\n"
-assert "cp diff.png localdiff.png"
-assert "./compare.sh TEST1/diff.png localdiff.png" "OK\n0\n"
+assert "cp TEST1/diff.png TEST1/localdiff.png"
+assert "./compare.sh TEST1/diff.png TEST1/localdiff.png" "OK\n0\n"
+assert "rm TEST1/localdiff.png"
 
 assert_end regression
 
